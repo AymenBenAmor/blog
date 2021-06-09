069154a7f39a094cf98a01c21a000b7453c4df68
Aymen Ben Amor <aymen.ben.amor@ing.com>
2021-06-09 22:41:34 +0200

    fix build issue

--- next.config.js
+++ next.config.js
@@ -5,10 +5,6 @@ const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
 });
 
-module.exports = withMDX({
-  pageExtensions: ['js', 'jsx', 'md', 'mdx'],
-});
-
 module.exports = withMDX(
   withCSS({
     webpack: (config, { isServer }) => {
@@ -19,6 +15,7 @@ module.exports = withMDX(
 
       return config;
     },
+    target: 'serverless',
     pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
   })
 );
